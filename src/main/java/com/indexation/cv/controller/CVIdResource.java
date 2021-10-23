ba8046573099b063d70f1d12aa8c47349395e135
Alexandre Em <71043267+alexandre-em@users.noreply.github.com>
2021-10-23 14:55:09 +0200

    [WIP] Generate a Swagger  (#13)
    * Add logs for uploadPDF route
    
    * Updated config.yml
    
    * [WIP] Add swagger api doc
    
    * Add swagger api doc

--- src/main/java/com/indexation/cv/controller/CVIdResource.java
+++ src/main/java/com/indexation/cv/controller/CVIdResource.java
@@ -3,6 +3,11 @@ package com.indexation.cv.controller;
 import com.indexation.cv.data.CVResponse;
 import com.indexation.cv.service.CVLogger;
 import com.indexation.cv.service.CVService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/cv/{id}")
+@Tag(name = "CV Details", description = "Get all information about a CV with an ID")
 public class CVIdResource {
     @Autowired
     private CVService cvService;
@@ -21,6 +27,8 @@ public class CVIdResource {
      * @param id
      * @return the content, the url of the CV
      */
+    @Operation(summary = "Get all details of a CV and its content")
+    @ApiResponse(responseCode = "200", description = "CV matching the id founded", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = CVResponse.class))})
     @GetMapping
     public ResponseEntity<CVResponse> searchById(@PathVariable("id") String id) {
         CVLogger.info("[GET] CV ID "+id);
